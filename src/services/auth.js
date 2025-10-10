62b127197ce0a39bb1e2c0e9f5b7212132ef4560
shivam <upadhayayshivam06@gmail.com>
2025-10-10 15:28:10 +0530

    fix: val

--- src/services/auth.js
+++ src/services/auth.js
@@ -7,7 +7,7 @@ export async function login(username, password) {
     { username, password }, // body (already content)
     {
       headers: {
-        "ark-plugin-secret-key": import.meta.env.VITE_ARK_PLUGIN_SECRET_KEY || "b29c957d8fd7dbfbae08fd8d9c143e3716f9af9b33e80b44deb7deaf41289e04",
+        "ark-plugin-secret-key": import.meta.env.VITE_ARK_PLUGIN_SECRET_KEY || "",
       },
     }
   );
